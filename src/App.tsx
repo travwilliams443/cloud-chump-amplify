c472c76caa1beb0cb059b3689c01be042c253f3f
Travis <travwilliams@gmail.com>
2024-05-09 22:56:09 -0400

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>();
 
@@ -22,24 +24,29 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map(todo => <li
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://next-release-dev.d1ywzrxfkb9wgg.amplifyapp.com/react/start/quickstart/vite-react-app/#step-2-add-delete-to-do-functionality">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map(todo => <li
+              onClick={() => deleteTodo(todo.id)}
+              key={todo.id}>
+              {todo.content}
+            </li>)}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://next-release-dev.d1ywzrxfkb9wgg.amplifyapp.com/react/start/quickstart/vite-react-app/#step-2-add-delete-to-do-functionality">
+              Review next step of this tutorial.
+            </a>
+          </div>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
